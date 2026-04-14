d03301c881d865d0e5d532d2ed15685f5e966d0b
aarov7 <aarovp2@gmail.com>
2026-04-14 19:41:58 +0800

    setup with uv

--- README.md
+++
@@ -1,2 +0,0 @@
-# ANAIprosperity
-Repo for ANAI Strategies team's prosperity 4 challenge
---
+++ main.py
@@ -0,0 +1,7 @@
+import jsonpickle
+def main():
+    print("Hello from anaiprosperity!")
+
+
+if __name__ == "__main__":
+    main()
